c722e0104fd11badd4217b59b5fe8e38dbdab591
WatsonCIQ <chris.watson@chartiq.com>
2020-07-24 15:28:33 +0100

    adding channel matcher types

---
+++ src/services/FDC3ChannelMatcherService/FDC3ChannelMatcherTypes.d.ts
@@ -0,0 +1,24 @@
+interface Listener {
+  unsubscribe(): void;
+}
+
+interface ProviderChannel {
+  inbound: string | null;
+  outbound: string | null;
+}
+
+interface Provider {
+  [providerName: string]: ProviderChannels
+}
+
+interface ProviderChannels {
+  [providerFDC3ChannelName: string]: ProviderChannel
+}
+interface ThirdPartyProviders {
+  providers: Provider
+}
+
+interface StateProviderChannel extends ProviderChannel {
+  inboundListener?: Listener;
+  outboundListener?: Listener;
+}
